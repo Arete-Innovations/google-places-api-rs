67c60504dfb841a88b003880985b56c2efde5fc9
croi <cristianalexandrucroitoru@gmail.com>
2025-02-13 11:52:12 +0200

    make find_place and place_photos public

--- src/endpoints/mod.rs
+++ src/endpoints/mod.rs
@@ -1,7 +1,7 @@
 pub mod api;
-mod find_place;
+pub mod find_place;
 pub mod nearby_search;
 pub mod place_details;
 pub mod place_search;
 pub mod text_search;
-mod place_photos;
+pub mod place_photos;
